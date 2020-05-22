40c8401781b725dac3e119261f0f1eb50574cb15
Zoltán Schmidt <zoltanschmidt@protonmail.com>
2020-05-22 03:30:29 +0200

    initialize Vue frontend project

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="My Robohash" src="./assets/my_robohash.png">
+    <LotCalc msg="Phalcon Lot Calculator"/>
+  </div>
+</template>
+
+<script>
+import LotCalc from './components/LotCalc.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LotCalc
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
